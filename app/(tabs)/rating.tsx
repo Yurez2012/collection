0cc0eb89330a81d1797ab5fc00bac424d8acda16
yuraludchak <yura.ludchak@ralabs.org>
2024-08-07 18:47:06 +0300

    rating

--- app/(tabs)/rating.tsx
+++ app/(tabs)/rating.tsx
@@ -1,7 +1,7 @@
 import {View, Text, StyleSheet, Pressable} from "react-native";
 import React, {useEffect, useState} from "react";
 import Colors from '@/constants/Colors';
-import {Stack} from "expo-router";
+import {router, Stack} from "expo-router";
 import UserHistory from "@/components/user/UserHistory";
 import HeaderFriend from "@/components/navigation/HeaderFriend";
 import api from "@/interceptor/api";
@@ -25,6 +25,9 @@ const Friend = () => {
         }
     };
 
+    const handlePressAsync = async () => {
+        router.replace('collection/store');
+    };
 
     return (
         <>
@@ -32,11 +35,11 @@ const Friend = () => {
                 header: () => <HeaderFriend/>
             }}/>
             <View style={styles.container}>
-                {category ? category.map(item => <View style={styles.category}>
+                {category ? category.map(item => <View style={styles.category} key={item.id}>
                     <Text style={styles.title}>
                         {item.title}
                     </Text>
-                    <Pressable style={styles.btn}>
+                    <Pressable onPress={handlePressAsync} style={styles.btn}>
                         <Text  style={styles.btn_title}>
                             Show rating
                         </Text>
