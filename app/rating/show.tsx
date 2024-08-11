2b5e83d4be63336c206611bee86e99808d33d6d9
yuraludchak <yura.ludchak@ralabs.org>
2024-08-11 18:40:31 +0300

    scroll

--- app/rating/show.tsx
+++ app/rating/show.tsx
@@ -1,18 +1,84 @@
-import {View, Text} from "react-native";
-import React from "react";
-import { useLocalSearchParams } from 'expo-router';
+import {View, Text, StyleSheet, ScrollView, TextInput} from "react-native";
+import React, {useEffect, useState} from "react";
+import {Stack, useLocalSearchParams} from 'expo-router';
+import api from "@/interceptor/api";
+import HeaderBack from "@/components/navigation/HeaderBack";
+import Colors from "@/constants/Colors";
 
 export default function Index() {
+    const {id} = useLocalSearchParams();
+    const [collections, setCollections] = useState(null);
 
-    const { id } = useLocalSearchParams();
+    useEffect(() => {
+        if (collections === null) {
+            getCollections();
+        }
+    }, [collections]);
 
-    console.log(id);
+    const getCollections = async () => {
+        try {
+            const response = await api.get('/rating/' + id);
+
+            setCollections(response.data.collections)
+        } catch (error) {
+            console.error('Axios error:', error);
+        }
+    };
+
+    const saveValue = async (id, value) => {
+        try {
+            const response = await api.put('/rating/' + id, {
+                rating: value
+            });
+
+            setCollections(response.data.collections)
+        } catch (error) {
+            console.error('Axios error:', error);
+        }
+    };
 
     return (
-        <View>
-            <Text>
-                asd
-            </Text>
-        </View>
+        <>
+            <Stack.Screen options={{
+                header: () => <HeaderBack/>
+            }}/>
+            <ScrollView style={styles.container}>
+                {
+                    collections ? collections.map(item => <View style={styles.cart} key={item.id}>
+                        <Text style={styles.title}>
+                            {item?.model?.title}
+                        </Text>
+                        <TextInput keyboardType='numeric' placeholder={`${item.rating}`} placeholderTextColor={Colors.licorice} onChangeText={(value) => saveValue(item.id, value)} style={styles.input} />
+                    </View>) : null
+                }
+            </ScrollView>
+        </>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        width: 50,
+        height: 50,
+        borderWidth: 1,
+        borderColor: Colors.licorice,
+        borderRadius: 10,
+        fontSize: 16,
+        color: Colors.licorice,
+        backgroundColor: Colors.slate_gray,
+        textAlign: 'center'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: Colors.slate_gray,
+        padding: 10
+    },
+    cart: {
+        marginBottom: 5,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    title: {
+        fontSize: 16
+    },
+})
